308e727ca135ba59d33a25f45a9df14a62b6b258
Haymanot Asmare <hasmare463@gmail.com>
2026-08-03 08:16:57 -0700

    refactor numpy array example to demonstrate 3D array access and remove commented-out code

--- py_numpy.py
+++ py_numpy.py
@@ -6,10 +6,19 @@ import numpy as np
 #3 dimensional
 # arr = np.array([1,2,3,4],ndmin=5)
 
-arr = np.array([1,2,3,4])
+#accessing 1-D elements of array
+# arr = np.array([1,2,3,4])
+
+# print(arr[2] + arr[3])
+
+arr = np.array([[[1,2,3,4],[5,6,7,8]],[[9,10,11,12],[13,14,15,16]]])
 
 print(arr)
-print(arr[2] + arr[3])
+
+# print(arr[0,1]) # accessing 2nd element of 1st row
+
+# print(arr[1,1]) # accessing 2nd element of 2nd row
+print(arr[1,1,2]) # accessing 3rd element of 2nd row of 2nd array
 
 # print('number of dimensions:',arr.ndim)
 # print(arr.ndim) # dimension
