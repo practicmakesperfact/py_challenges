893e152a14e390b7dddc60c8937feaa8695274d1
Haymanot Asmare <hasmare463@gmail.com>
2026-08-03 08:03:25 -0700

    refactor numpy array example and add dimension, shape, size, and dtype prints

--- py_numpy.py
+++ py_numpy.py
@@ -1,5 +1,16 @@
 import numpy as np
 
-print(np.__version__)
+# 2 dimensional array
+# arr = np.array([[1,2,3],[2,3,4]]) 
+
+#3 dimensional
+arr = np.array([1,2,3,4],ndmin=5)
 
 
+print(arr)
+print('number of dimensions:',arr.ndim)
+# print(arr.ndim) # dimension
+# print(arr.shape) # shape
+# print(arr.size) # total elements
+# print(arr.dtype) # data type
+# print(type(arr))
