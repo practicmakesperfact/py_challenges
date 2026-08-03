31f452ec180baec6b133d412a098837232fb5885
Haymanot Asmare <hasmare463@gmail.com>
2026-08-03 08:08:02 -0700

    accessing numpy and adding

--- py_numpy.py
+++ py_numpy.py
@@ -4,11 +4,14 @@ import numpy as np
 # arr = np.array([[1,2,3],[2,3,4]]) 
 
 #3 dimensional
-arr = np.array([1,2,3,4],ndmin=5)
+# arr = np.array([1,2,3,4],ndmin=5)
 
+arr = np.array([1,2,3,4])
 
 print(arr)
-print('number of dimensions:',arr.ndim)
+print(arr[2] + arr[3])
+
+# print('number of dimensions:',arr.ndim)
 # print(arr.ndim) # dimension
 # print(arr.shape) # shape
 # print(arr.size) # total elements
