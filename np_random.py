43a697c22cbd0e5a4deaa2d6a2ef454c88ef74c7
Haymanot Asmare <hasmare463@gmail.com>
2026-08-05 12:37:43 -0700

    generate random number in numpy

---
+++ np_random.py
@@ -0,0 +1,10 @@
+from numpy import random
+
+#generate random float numbers between 0 and 1
+# x = random.rand(6)
+# print(x)
+
+#generate random numbers from array
+
+x = random.choice([3, 5, 7, 9],size=(3,4))
+print(x)
