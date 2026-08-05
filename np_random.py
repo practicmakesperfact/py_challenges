9751169a7ed68b4992322e10ffad164fa0c8b26a
Haymanot Asmare <hasmare463@gmail.com>
2026-08-05 13:40:40 -0700

    use seaborn to visualize distribution

--- np_random.py
+++ np_random.py
@@ -1,5 +1,5 @@
 from numpy import random
-
+import numpy as np
 #generate random float numbers between 0 and 1
 # x = random.rand(6)
 # print(x)
@@ -10,7 +10,17 @@ from numpy import random
 # print(x)
 
 
-#probability distribution
+#probability distribution function(pdf)
 
-x = random.choice([4,7,3,9],p=[0.1,0.3,0.6,0.0],size=(3,4))
-print(x)
+# x = random.choice([4,7,3,9],p=[0.1,0.3,0.6,0.0],size=(3,4))
+# print(x)
+
+# import numpy as np
+# arr = random.shuffle(np.array([3,4,6,7,8,9]))
+# print(arr)  
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(np.array([0,1,2,3,4,5]), kind="kde")
+plt.show()
