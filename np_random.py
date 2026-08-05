92a43a86de3ec1d2ab604b7c6822e3826779b67e
Haymanot Asmare <hasmare463@gmail.com>
2026-08-05 13:05:01 -0700

    pdf

--- np_random.py
+++ np_random.py
@@ -6,5 +6,11 @@ from numpy import random
 
 #generate random numbers from array
 
-x = random.choice([3, 5, 7, 9],size=(3,4))
+# x = random.choice([3, 5, 7, 9],size=(3,4))
+# print(x)
+
+
+#probability distribution
+
+x = random.choice([4,7,3,9],p=[0.1,0.3,0.6,0.0],size=(3,4))
 print(x)
