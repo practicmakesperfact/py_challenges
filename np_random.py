df29f7b5efd795f7f0b00968a27a2bb12b3fe249
Haymanot Asmare <hasmare463@gmail.com>
2026-08-08 12:26:10 -0700

    uniform distribution in numpy

--- np_random.py
+++ np_random.py
@@ -23,12 +23,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+
 # sns.displot(random.normal(size=1000), kind="kde")
 # plt.show()
 
-data ={
-    "normal": random.normal(loc=50, scale=5, size=1000),
-    "binomial": random.binomial(n=100, p=0.5, size=1000)
-}
-sns.displot(data=data, kind="kde")
-plt.show()
+# data ={
+#     "normal": random.normal(loc=50, scale=5, size=1000),
+#     "binomial": random.binomial(n=100, p=0.5, size=1000)
+# }
+# sns.displot(data=data, kind="kde")
+# plt.show()
+
+x = random.uniform(low=0.02, high=3, size=(2,3))
+print(x)
